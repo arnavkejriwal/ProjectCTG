20992e8107492c724dbd6c76ca9392f235d8f9c1
tulsyanlakshya09 <127251539+tulsyanlakshya009@users.noreply.github.com>
2024-08-24 14:50:31 +0800

    WIP event post & put backend

--- backend/controllers/events.js
+++ backend/controllers/events.js
@@ -9,11 +9,67 @@ exports.get_events = async (req, res) => {
     }
 }
 
-exports.get_events_by_user = async (req, res) => {
-    const { user_id } = req.params;
+// exports.get_events_by_user = async (req, res) => {
+//     const { user_id } = req.params;
+//     try {
+//         const eventsData = await Event.find({ user_id });
+//         res.json(eventsData);
+//     } catch (error) {
+//         res.status(400).json({message: error.message});
+//     }
+// }
+
+exports.create_event = async (req, res) => {
+    const { title, description, event_id, emoji, image, subtitle, date, location, organiser, organiser_img, banner_img, volunteer_vacancies, participant_vacancies } = req.body;
+    const newEvent = new Event({
+        _id: mongoose.Schema.Types.ObjectId,
+        event_id,
+        emoji,
+        title,
+        image,
+        subtitle,
+        date,
+        location,
+        description,
+        organiser,
+        organiser_img,
+        banner_img,
+        volunteer_vacancies,
+        participant_vacancies
+    });
+    
     try {
-        const eventsData = await Event.find({ user_id });
-        res.json(eventsData);
+        await newEvent.save();
+        res.status(201).json(newEvent);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
+
+exports.update_event = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, event_id, emoji, image, subtitle, date, location, organiser, organiser_img, banner_img, volunteer_vacancies, participant_vacancies } = req.body;
+    if (!mongoose.Types.ObjectId.isValid(id)) { 
+        return res.status(404).json({error: 'No such event'});
+    }
+    const updatedEvent = {
+        _id: id,
+        event_id,
+        emoji,
+        title,
+        image,
+        subtitle,
+        date,
+        location,
+        description,
+        organiser,
+        organiser_img,
+        banner_img,
+        volunteer_vacancies,
+        participant_vacancies
+    };
+    try {
+        await Event.findByIdAndUpdate(id, updatedEvent, {new: true});
     } catch (error) {
         res.status(400).json({message: error.message});
     }
--- backend/routes/events.js
+++ backend/routes/events.js
@@ -2,14 +2,21 @@ const express = require('express');
 
 const {
     get_events,
-    get_events_by_user
+    // get_events_by_user,
+    create_event,
+    update_event
 } = require('../controllers/events');
+const { create } = require('../models/events');
 // const { get } = require('mongoose');
 
 const router = express.Router();
 
 router.get('/get_events', get_events);
 
-router.get('/get_events_by_user/user_id==:user_id', get_events_by_user);
+// router.get('/get_events_by_user/:user_id', get_events_by_user);
+
+router.post('/create_event', create_event);
+
+router.patch('/update_event/:id', update_event);
 
 module.exports = router
