38698363befe31798dbcb25b26dbf90df8d9b705
Marcus <marcusko.work@gmail.com>
2024-08-24 22:05:16 +0800

    remove comments

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,18 +1,3 @@
-// import React from 'react';
-
-// export default function Quiz({ quiz }){
-//   return (
-//     <div className="quiz-card">
-//       <img src={quiz.image} alt={quiz.title} className="quiz-image" />
-//       <div className="quiz-content">
-//         <h3 className="quiz-title">{quiz.title}</h3>
-//         <p className="quiz-description">{quiz.description}</p>
-//         <button className="quiz-button">Take Quiz</button>
-//       </div>
-//     </div>
-//   );
-// };
-
 import React, { useState } from 'react';
 import { Box, Typography, Button } from '@mui/material';
 
