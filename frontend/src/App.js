047e6e1b38e5a4aecc30a01ff9172b2d80588ce0
Vedika Saini <vedikasaini10@gmail.com>
2024-08-24 13:04:51 +0800

    Added quiz pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Navbar from './components/Navbar'
 import Event from './pages/Event'
 import Profile from './pages/Profile'
 import Admin from './pages/Admin'
+import Quizzes from './pages/Quizzes'
 
 function App() {
   const { user } = useAuthContext()
@@ -42,6 +43,10 @@ function App() {
             <Route 
               path="/profile" 
               element={<Profile />} 
+            />
+            <Route 
+              path="/quizzes" 
+              element={<Quizzes />} 
             />
           </Routes>
         </div>
